d4be927fdb60b0a4c059be7eaf95fc8530413d83
Itai Benjamin <51812134+itaibenjy@users.noreply.github.com>
2023-06-14 15:14:58 +0300

    finishing fractal trees

--- src/hooks/useFractal.js
+++ src/hooks/useFractal.js
@@ -9,11 +9,15 @@ export function useFractal() {
     const contextRef = useRef(null);
 
     const [angle, setAngle] = useState(30)
+    const [depth, setDepth] = useState(11)
+    const [initialSize, setInitialSize] = useState(0.17)
+    const [reducer , setReducer] = useState(0.78)
+    const [isLeafs, setIsLeafs] = useState(false)
   
 
 
     // This function will be recreated whenever the canvas context changes.
-    const drawLine = useCallback((x1Percent, y1Percent, x2Percent, y2Percent) => {
+    const drawLine = useCallback((x1Percent, y1Percent, x2Percent, y2Percent, lineWidth) => {
       if (contextRef.current) {
         const width = canvasRef.current.width;
         const height = canvasRef.current.height;
@@ -29,29 +33,56 @@ export function useFractal() {
   
         context.moveTo(x1, y1); 
         context.lineTo(x2, y2); 
-  
+
         context.strokeStyle =  theme === "dark" ? "#332d2d" : "#fbfbfb"; // sets the color to fill
-        context.lineWidth = 5; // sets the line width
+        context.lineWidth = lineWidth; // sets the line width
         context.stroke(); // applies the stroke
   
         context.closePath(); // end
       }
-    }, []);
+    }, [theme]);
+
+
+    const drawLeaf = useCallback((xPercent, yPercent, size, angle) => {
+      const width = canvasRef.current.width;
+      const height = canvasRef.current.height;
+
+      const x = xPercent * width;
+      const y = yPercent * height;
+
+      const context = contextRef.current;
+      context.save(); // save the current context state
+      context.translate(x, y); // move the context to the center of the leaf
+      context.rotate(angle * Math.PI / 180); // rotate the context by the angle
+      context.beginPath();
+      context.moveTo(0, 0);
+      context.quadraticCurveTo(-size, -size, -size / 2, -size * 2);
+      context.quadraticCurveTo(0, -size * 4, size / 2, -size * 2);
+      context.quadraticCurveTo(size, -size, 0, 0);
+      context.lineTo(0, -size * 3); // add a line in the middle of the leaf
+      context.fillStyle = "green";
+      context.fill();
+      context.strokeStyle =  theme === "dark" ? "#332d2d" : "#fbfbfb"; // sets the color to fill
+      context.lineWidth = size / 20;
+      context.stroke();
+      context.closePath();
+      context.restore(); // restore the previous context state
+    }, [theme]);
 
     useEffect(() => {
         clearCanvas()
-        console.log(angle)
-        drawTree(0.5,1,-90,angle,0.3,9)
-    }, [angle])
+        drawTree(0.5,1,-90,angle, initialSize, reducer, depth)
+    }, [angle, theme, depth, initialSize, reducer, isLeafs])
 
-    function drawTree(x1, y1, angle, rotate, size, depth) {
-        if (depth === 0) return;
+    function drawTree(x1, y1, initialAngle, rotate, size, reducer, treeDepth, lineWidth = 5) {
+        if (treeDepth === 0)  return;
         // make the x2 and y2 be between 0 and 1 for precetage representation
-        const x2 = x1 + (Math.cos(angle * Math.PI / 180) * size);
-        const y2 = y1 + (Math.sin(angle * Math.PI / 180) * size);
-        drawLine(x1, y1, x2, y2);
-        drawTree(x2, y2, angle - rotate, rotate, size/1.5, depth - 1);
-        drawTree(x2, y2, angle + rotate, rotate, size/1.5, depth - 1);
+        const x2 = x1 + (Math.cos(initialAngle * Math.PI / 180) * size);
+        const y2 = y1 + (Math.sin(initialAngle * Math.PI / 180) * size);
+        drawLine(x1, y1, x2, y2, lineWidth);
+        drawTree(x2, y2, initialAngle - rotate, rotate, size*reducer, reducer, treeDepth - 1, lineWidth*reducer);
+        drawTree(x2, y2, initialAngle + rotate, rotate, size*reducer, reducer, treeDepth - 1, lineWidth*reducer);
+        if (treeDepth === 1 && isLeafs) drawLeaf(x2, y2, size*500, initialAngle+90);
     }
 
 
@@ -65,7 +96,8 @@ export function useFractal() {
 
 
     const canvasProps = {drawLine, canvasRef, contextRef, clearCanvas}
+    const treeProps = {angle, setAngle, depth, setDepth, initialSize, setInitialSize, reducer, setReducer, isLeafs, setIsLeafs}
 
 
-    return {canvasProps, angle, setAngle}
+    return {canvasProps, treeProps}
 }
--- src/pages/FractalTree.js
+++ src/pages/FractalTree.js
@@ -1,25 +1,65 @@
 
-import { MDBBtn, MDBContainer, MDBRange, MDBTypography, MDBIcon, MDBTooltip } from "mdb-react-ui-kit";
+import { MDBBtn, MDBContainer, MDBRange, MDBTypography, MDBIcon, MDBTooltip, MDBCheckbox } from "mdb-react-ui-kit";
 import FractalCanvas from "../components/FractalTree/FractalCanvas";
 import {useFractal} from "../hooks/useFractal"
 
 export default function FractalTree() {
 
-    const {canvasProps, angle, setAngle} = useFractal()
+    const {canvasProps, treeProps} = useFractal()
 
     return (<MDBContainer className="p-1 mb-5 pageContainer">
-    <MDBTypography tag='h1' style={{fontFamily: 'Monomania'}} className='display-1'>Fractal Trees</MDBTypography>
-        <MDBContainer  className="d-flex justify-content-center my-5">
-            <FractalCanvas {...canvasProps}/>
+        <MDBTypography tag='h1' style={{fontFamily: 'Monomania'}} className='display-1'>Fractal Trees</MDBTypography>
+        <MDBContainer id="Two-cards" className=" my-5">
+            <MDBContainer  className="d-flex justify-content-center mb-4">
+                <FractalCanvas {...canvasProps} className="mx-auto"/>
+            </MDBContainer> 
+            <MDBContainer  className="card mx-4">
+                <MDBTypography tag='h6' style={{fontFamily: 'Monomania'}} className='display-6 text-center'>Tree Properties</MDBTypography>
+                <MDBContainer  className="my-3">
+                    <MDBCheckbox label="Leaves" checked={treeProps.isLeafs} onChange={() => treeProps.setIsLeafs(!treeProps.isLeafs)}/>
+                </MDBContainer>
+                <MDBContainer  className="mb-5">
+                    <MDBRange
+                        max={180}
+                        min={0}
+                        defaultValue={treeProps.angle}
+                        onChange={(e) => treeProps.setAngle(Number(e.target.value))}
+                        id='angleRange'
+                        label='Angle'
+                    />
+                </MDBContainer>
+                <MDBContainer  className="my-5">
+                    <MDBRange
+                        max={12}
+                        min={1}
+                        defaultValue={treeProps.depth}
+                        onChange={(e) => treeProps.setDepth(Number(e.target.value))}
+                        id='branchRange'
+                        label='Branches'
+                    />
+                </MDBContainer>
+                <MDBContainer  className="my-5">
+                    <MDBRange
+                        max={100}
+                        min={1}
+                        defaultValue={treeProps.reducer*100}
+                        onChange={(e) => treeProps.setReducer(Number(e.target.value)/100)}
+                        id='reducerRange'
+                        label='Branch Size Reducer (Percent)'
+                    />
+                </MDBContainer>
+                <MDBContainer  className="my-5">
+                    <MDBRange
+                        max={100}
+                        min={1}
+                        defaultValue={treeProps.initialSize*100}
+                        onChange={(e) => treeProps.setInitialSize(Number(e.target.value)/100)}
+                        id='sizeRange'
+                        label='Initial Branch Size (Percent of Canvas)'
+                    />
+                </MDBContainer>
+            </MDBContainer>
         </MDBContainer>
-            <MDBRange
-                max={90}
-                min={0}
-                defaultValue={angle}
-                onChange={(e) => setAngle(Number(e.target.value))}
-                id='angleRange'
-                label='Angle'
-            />
     </MDBContainer>
     );
 }
